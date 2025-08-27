45b65f418aea96c91d1684b24353e295dedc1055
Patrick <patrickcosta1605@gmail.com>
2025-08-27 07:59:59 +0100

    Adiciona base de dados pool_budgets.db para deploy correto

--- db/add_sacos_areia.sql
+++
@@ -1,4 +0,0 @@
--- Inserção de sacos de areia fina e grossa para filtros de piscina
-INSERT INTO products (name, base_price, unit, category_id, is_active) VALUES
-('Areia granulada para filtro de piscina - granulometria 0,4-1,0mm (Saco de 25kg)', 12.90, 'saco', 5, 1),
-('Areia granulada para filtro de piscina - granulometria 1,5-3,0mm (Saco de 25kg)', 12.90, 'saco', 5, 1);
